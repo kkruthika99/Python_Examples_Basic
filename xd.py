0e3f7727c12c6c1fd6dc8b5897a5976ede882ecb
Kruthika K <kruthikakalmali@gmail.com>
2021-10-27 00:09:05 +0530

    Create xd.py

---
+++ xd.py
@@ -0,0 +1,14 @@
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
